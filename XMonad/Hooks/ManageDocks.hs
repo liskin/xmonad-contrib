liskin/xmonad-contrib

commit ec146171238b10eacc220bef3d6d2123ff27d3ad

X.H.ManageDocks: React to strut updates of override_redirect docks
We only requested PropertyChange events from docks in the `manageDocks`
manageHook, but that only gets called for normal windows, not
override_redirect ones. Therefore, xmobar in its default configuration
wouldn't get its struts refreshed on changes. This resulted in gaps not
updating after xmobar repositions itself on xrandr changes.

If one wanted to handle that repositioning in xmonad, it was possible to
configure xmobar with `overrideRedirect = False`, which is meant for
window managers with proper EWMH stacking order support [1], so in
xmonad it resulted in xmobar covering fullscreen windows. That can be
worked around by adding `checkDock --> doLower` to manageHook, but it
starts to smell of too many workarounds.

[1]: https://specifications.freedesktop.org/wm-spec/wm-spec-1.5.html#STACKINGORDER

The fix is to request PropertyChange events for all windows that we
treat as docks.

Related: https://github.com/xmonad/xmonad-contrib/pull/490

XMonad/Hooks/ManageDocks.hs
@@ -130,17 +130,22 @@ maybeInitStrutCache = maybe (queryDocks >>= foldlM (flip updateStrut) M.empty) p
 
 updateStrut :: Window -> WindowStruts -> X WindowStruts
 updateStrut w cache = do
+    when (w `M.notMember` cache) $ requestDockEvents w
     strut <- getStrut w
     pure $ M.insert w strut cache
 
 -- | Detects if the given window is of type DOCK and if so, reveals
 --   it, but does not manage it.
 manageDocks :: ManageHook
-manageDocks = checkDock --> (doIgnore <+> setDocksMask)
-    where setDocksMask = do
-            ask >>= \win -> liftX $ withDisplay $ \dpy ->
-                io $ selectInput dpy win (propertyChangeMask .|. structureNotifyMask)
-            mempty
+manageDocks = checkDock --> (doIgnore <+> doRequestDockEvents)
+  where
+    doRequestDockEvents = ask >>= liftX . requestDockEvents >> mempty
+
+-- | Request events for a dock window.
+-- (Only if not already a client to avoid overriding 'clientMask')
+requestDockEvents :: Window -> X ()
+requestDockEvents w = whenX (not <$> isClient w) $ withDisplay $ \dpy ->
+    io $ selectInput dpy w (propertyChangeMask .|. structureNotifyMask)
 
 -- | Checks if a window is a DOCK or DESKTOP window
 checkDock :: Query Bool

commit 44aefa685ba396c81ae23824bd0087accec0ad0f

X.H.ManageDocks: Fix button events on decoration windows
Decoration windows are created using XMonad.Util.XUtils.createNewWindow
which happens to set _NET_WM_WINDOW_TYPE to _NET_WM_WINDOW_TYPE_DESKTOP,
and ManageDocks considers such windows candidates for struts and
therefore requests property events, thus overriding the original event
mask requested by decorations.

The fix is to first obtain the current event mask, set the required bits
in it only then reset the mask.

Fixes: https://github.com/xmonad/xmonad-contrib/issues/517
Fixes: ec146171238b ("X.H.ManageDocks: React to strut updates of override_redirect docks")
Related: https://github.com/xmonad/X11/pull/77

XMonad/Hooks/ManageDocks.hs
@@ -145,7 +145,8 @@ manageDocks = checkDock --> (doIgnore <+> doRequestDockEvents)
 -- (Only if not already a client to avoid overriding 'clientMask')
 requestDockEvents :: Window -> X ()
 requestDockEvents w = whenX (not <$> isClient w) $ withDisplay $ \dpy ->
-    io $ selectInput dpy w (propertyChangeMask .|. structureNotifyMask)
+    withWindowAttributes dpy w $ \attrs -> io $ selectInput dpy w $
+        wa_your_event_mask attrs .|. propertyChangeMask .|. structureNotifyMask
 
 -- | Checks if a window is a DOCK or DESKTOP window
 checkDock :: Query Bool